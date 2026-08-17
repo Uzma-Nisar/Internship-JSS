b16ad9f40e2d324ff93b561aba661b807241fd58
Uzma <uzmachaudhary301@gmail.com>
2026-08-17 18:02:09 +0500

    fix exact case for logo import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Navbar.css'; 
-import logo from '../assets/logo.png'; 
+import logo from '../assets/LOGO.png'; 
 import PricingModal from '../pages/PricingModal';
 
 const Navbar = () => {
